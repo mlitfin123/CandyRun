6ae244581381c17715b86aff78819d7a466d7fd0
Mark <mark.litfin@yahoo.com>
2020-05-03 18:10:09 -0400

    Adding Game Files

---
+++ Assets/Scripts/CardFlip.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CardFlip : MonoBehaviour
+{
+    private Sprite[] cards;
+    private SpriteRenderer rend;
+    private AudioSource cardFlipper;
+    void Awake()
+    {
+        cardFlipper = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        cards = Resources.LoadAll<Sprite>("Cards/");
+        rend.sprite = cards[16];
+    }
+
+    public void Function()
+    {
+        if (!GameControl.gameOver)
+            StartCoroutine("FlipTheCard");
+    }
+
+    private IEnumerator FlipTheCard()
+    {
+        int randomCard = 0;
+        for (int i = 0; i <= 20; i++)
+        {
+            randomCard = Random.Range(0, 16);
+            rend.sprite = cards[randomCard];
+            yield return new WaitForSeconds(0.05f);
+            cardFlipper.Play();
+        }
+        Debug.Log(randomCard);
+    }
+}
